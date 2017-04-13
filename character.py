23e25b79cac5c12f7f67300c6447bc697377adb4
sharef88 <andy@orbanos.org>
2017-04-12 23:53:04 -0400

    Inject ref variables

--- character.py
+++ character.py
@@ -2,8 +2,13 @@
 '''
 Module for the definition of an SR5 character
 '''
+import json
 
 class Character(object):
+    attributes_ref = ('body', 'agility', 'reaction', 'strength',
+                      'willpower', 'logic', 'intuition', 'charisma',
+                      'essence')
+    active_skill_ref = json.load(open('./skills.json'))
     def __init__(self, *initial_data, **kwargs):
         for obj in initial_data:
             for key in obj:
